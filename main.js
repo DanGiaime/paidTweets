d85834751cb2fe2b9739068b17718cfb4e43f22f
Dan Giaime <dsg6874@rit.edu>
2021-01-17 05:22:52 -0600

    just take users that retweeted

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 const puppeteer = require('puppeteer');
+const {fullLoginFlow, sleep} = require('./pages/login');
+const {USERNAME, PASSWORD} = require('./secrets');
+const {scrollUntilFindUsernames} = require('./pages/singleTweet');
+
+
+let makeScreenPhoneSize = async (page) => {
+  await page.setViewport({ width: 660, height: 1000 });
+};
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto('https://twitter.com');
-  await page.screenshot({path: 'twitter.png'});
+  const tweetURL = "https://twitter.com/roxkstar74/status/1346145124686946304/retweets";
+
+  // load login page as phone
+  await page.goto('https://www.twitter.com/login');
+  await makeScreenPhoneSize(page);
+
+  // login to our account
+  await fullLoginFlow(page, USERNAME, PASSWORD);
+  await sleep(2000);
+
+  // navigate to the actual tweet page retweets
+  // await page.goto(tweeterProfile);
+  await page.goto(tweetURL);
+  await sleep(2000);
+
+  let usernames = await scrollUntilFindUsernames(page);
+  // console.log(postLink);
+  // let tweetInfo = await getAllTweetInfo(page);
+  console.log(usernames);
+  await sleep(2000);
+
+
+
+  // pull all needed info from the page
+
 
   await browser.close();
 })();
--- pages/singleTweet.js
+++ pages/singleTweet.js
@@ -28,34 +28,37 @@ let getAllTweetInfo = async (page, desiredAPath) => {
     };
 }
 
-let scrollUntilFindPath = async (page, desiredAPath) => {
+let scrollUntilFindUsernames = async (page) => {
     
 
-    return await page.evaluate(async (desiredAPath) => {
+    return await page.evaluate(async () => {
         for(let i = 0; i < 20; i++) {
             window.scrollBy(0, window.innerHeight);
             await new Promise(resolve => setTimeout(resolve, 1000));
             // First indication of finding the right tweet on the profile
-            let postLink = document.querySelector(`a[href="${desiredAPath}"]`);
+            // let postLink = document.querySelector(`a[href="${desiredAPath}"]`);
 
-            // go up to the parent of the posts so that we an go back down to find the retweet and like info
-            if(postLink) {
-                let dataEncapsulatingElement = document.querySelector(`a[href="${desiredAPath}"]`).parentElement.parentElement.parentElement.parentElement.parentElement.children[1].children[2];
-                return {
-                    retweets: Array.from(dataEncapsulatingElement.querySelectorAll('[data-testid="retweet"]'), ({ textContent }) => textContent),
-                    replies: Array.from(dataEncapsulatingElement.querySelectorAll('[data-testid="reply"]'), ({ textContent }) => textContent),
-                    likes: Array.from(dataEncapsulatingElement.querySelectorAll('[data-testid="like"]'), ({ textContent }) => textContent),
-                }
-            };
+            // // go up to the parent of the posts so that we an go back down to find the retweet and like info
+            // if(postLink) {
+            //     let dataEncapsulatingElement = document.querySelector(`a[href="${desiredAPath}"]`).parentElement.parentElement.parentElement.parentElement.parentElement.children[1].children[2];
+            //     return {
+            //         retweets: Array.from(dataEncapsulatingElement.querySelectorAll('[data-testid="retweet"]'), ({ textContent }) => textContent),
+            //         replies: Array.from(dataEncapsulatingElement.querySelectorAll('[data-testid="reply"]'), ({ textContent }) => textContent),
+            //         likes: Array.from(dataEncapsulatingElement.querySelectorAll('[data-testid="like"]'), ({ textContent }) => textContent),
+            //     }
+            // };
 
-            
+            // return Array.from(dataEncapsulatingElement.querySelectorAll('[data-testid="retweet"]'), ({ textContent }) => textContent)
         }
-    }, desiredAPath);
+
+        // return list of usernames
+        return Array.from(document.querySelectorAll('div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs > a > div > div.css-1dbjc4n.r-18u37iz.r-1wbh5a2 > div > span')).map(x => x.textContent).filter(x => x.indexOf("@") > -1);
+    });
 
     
 }
 
 module.exports = {
     getAllTweetInfo,
-    scrollUntilFindPath
+    scrollUntilFindUsernames
 }
